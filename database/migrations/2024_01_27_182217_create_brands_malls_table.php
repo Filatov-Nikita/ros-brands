f4be20f5b24affeb66dabec2a5df17ea7b6939e2
Nikita Antonov <nikita45454@gmail.com>
2024-01-27 21:33:11 +0300

    добавляет миграцию brands_malls

---
+++ database/migrations/2024_01_27_182217_create_brands_malls_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands_malls', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('brand_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreignId('mall_id')->constrained()
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands_malls');
+    }
+};
