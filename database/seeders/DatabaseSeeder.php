56f6d960dff2ae93ee12a7975a77dad2b70f6028
Nikita Antonov <nikita45454@gmail.com>
2024-01-31 12:44:36 +0300

    добавляет модель и сидер LookCategory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             BrandSeeder::class,
             ProductCategorySeeder::class,
             LookColorSeeder::class,
+            LookCategorySeeder::class,
         ]);
     }
 }
