4ee6d82d163527b4822db672ca2749e03db0e53a
Nikita Antonov <nikita45454@gmail.com>
2024-02-01 12:09:41 +0300

    добавляет модель и сидер баннера

---
+++ database/seeders/BannerSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Banner;
+
+class BannerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Banner::create([
+            'title' => 'Акция от zara',
+            'href' => 'https://ya.ru',
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             LookCategorySeeder::class,
             LookStyleSeeder::class,
             DesignerSeeder::class,
+            BannerSeeder::class,
         ]);
     }
 }
