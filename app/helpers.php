f2017c4ad11a61b41c6eccba2934e1b10f22a443
Nikita Antonov <nikita45454@gmail.com>
2024-02-06 20:56:20 +0300

    feat: добавляет апи контроллер для категорий товаров

--- app/helpers.php
+++ app/helpers.php
@@ -12,3 +12,14 @@ function app_get_tree($cats, $parent_id = null) {
 
     return $list;
 }
+
+function get_all_parent_cat_ids($cats, $cat_id, & $list = []) {
+    $list[] = $cat_id;
+    foreach($cats as $cat) {
+        if($cat['parent_id'] === $cat_id) {
+            get_all_parent_cat_ids($cats, $cat['id'], $list);
+        }
+    }
+
+    return $list;
+}
