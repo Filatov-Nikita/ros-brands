2ba353bb39540a84cb8dc9d43b544a5df8d5ca6b
Nikita Antonov <nikita45454@gmail.com>
2024-02-11 13:38:18 +0300

    feat: добавляет связи для картинок в модель Look

--- app/Models/Look.php
+++ app/Models/Look.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Look extends Model
 {
@@ -36,5 +38,15 @@ class Look extends Model
     public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class);
+    }
+
+    public function thumbnail(): MorphOne
+    {
+        return $this->morphOne(Attachment::class, 'domain')->where('type', 'thumbnail');
+    }
+
+    public function images(): MorphMany
+    {
+        return $this->morphMany(Attachment::class, 'domain')->where('type', 'image');
     }
 }
